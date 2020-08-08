14a64809e7d6f5d88bdb72114095c8dbdd4b3edd
divned <40516167+divned@users.noreply.github.com>
2020-08-09 01:25:13 +0530

    Add files via upload

---
+++ Hello_Div.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Getting Started With Anaconda 
+# ##Practical activiy
+# In this practice activity / quiz, please:
+# 
+# Create a new virtual environment
+# Calling it "irisproject"
+# Ensure that Anaconda, Jupyter Notebook, and Spyder has been successfully installed
+# Do make sure you have successfully completed the above before you proceed to the next portion. We will need it for the next lecture.
+
+# In[1]:
+
+
+print("Hello Div")
+
+
+# In[2]:
+
+
+1+1
+
+
+# In[11]:
+
+
+var=5
+
+
+# In[14]:
+
+
+1+1
+var
+
+
+# In[15]:
+
+
+print(1+1)
+print(var)
+
+
+# ## To enter formuals
+# note: this syntax only work in "Markdown" not in the "Code"
+
+# $x=\frac{1}{7}$ , $y=1$
+
+# In[10]:
+
+
+4**5
+
